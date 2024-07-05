b5f8c1172a4fbf21f50486801ca23a01421c8065
flouter <80354346+flouterrr@users.noreply.github.com>
2024-07-05 23:35:42 +0200

    Delete build directory

--- build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/dev/c_projects/raycaster/build/_deps/raylib-src/src
-# Build directory: D:/dev/c_projects/raycaster/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
