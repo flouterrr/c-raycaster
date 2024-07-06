1749ef7df8ab3dd6961bfb377763e8907027570a
flouter <jonasz.b04@gmail.com>
2024-07-06 18:49:25 +0200

    simplify build system

--- build.bat
+++ build.bat
@@ -1,30 +1,29 @@
 @echo off
 set "proj_name=raycaster"
+set "compiler=gcc"
+set "generator=Ninja"
 goto Start
 
 :Start
-goto FreshCheck
-
-:FreshCheck
-if "%~1"=="-f" goto Fresh
-if "%~2"=="-f" goto Fresh
-goto NotFresh
-
-:Fresh
-@echo on
-cmake -S . -B .\build -G "Ninja" -Wdev -DCMAKE_C_COMPILER=gcc --fresh
 @echo off
-goto Build
+if "%~1"=="-d" goto SetDebug
+if "%~2"=="-d" goto SetDebug
+set "cmake_build_type=Release"
+goto Configure
 
-:NotFresh
+:SetDebug
+set "cmake_build_type=Debug"
+goto Configure
+
+:Configure
 @echo on
-cmake -S . -B .\build -G "Ninja" -Wdev -DCMAKE_C_COMPILER=gcc
+cmake -S . -B .\build\%cmake_build_type% -G"%generator%" -Wdev -DCMAKE_C_COMPILER=%compiler% -DCMAKE_BUILD_TYPE=%cmake_build_type%
 @echo off
 goto Build
 
 :Build
 @echo on
-cmake --build .\build -v
+cmake --build .\build\%cmake_build_type% -v
 @echo off
 goto RunCheck
 
@@ -32,8 +31,10 @@ goto RunCheck
 if "%~1"=="-r" goto Run
 if "%~2"=="-r" goto Run
 goto SkipRun
+
 :Run
 @echo on
-.\build\%proj_name%
+.\build\%cmake_build_type%\%proj_name%
 @echo off
+
 :SkipRun
